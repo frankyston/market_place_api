365706d8e5ea3314e99f5a7099c8f3ccbd95ffc1
Frankyston Lins <frankyston@gmail.com>
2019-12-31 16:57:15 -0300

    Adds caching for the serializers

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,6 @@
 class OrderSerializer
   include FastJsonapi::ObjectSerializer
+  cache_options enabled: true, cache_length: 12.hours
   belongs_to :user
-  has_many :products 
+  has_many :products
 end
