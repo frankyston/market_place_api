5897080c581e5ee47879b3e0d4ab56783d3c4bc7
Frankyston Lins <frankyston@gmail.com>
2019-12-31 16:54:03 -0300

    Factorize pagination

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -10,10 +10,7 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     json_response = JSON.parse(response.body, symbolize_names: true)
-    assert_not_nil json_response.dig(:links, :first)
-    assert_not_nil json_response.dig(:links, :last)
-    assert_not_nil json_response.dig(:links, :prev)
-    assert_not_nil json_response.dig(:links, :next)
+    assert_json_response_is_paginated json_response
   end
 
   test 'should show product' do
@@ -24,8 +21,6 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal @product.title, json_response.dig(:data, :attributes, :title)
     assert_equal @product.user.id.to_s, json_response.dig(:data, :relationships, :user, :data, :id)
     assert_equal @product.user.email, json_response.dig(:included, 0, :attributes, :email)
-
-    
   end
 
   test 'should create product' do
